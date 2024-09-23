7f6fe9d5aa2b5cd16ecf808c0230cdbb078edbd5
Prashant Channe <prashantchanne121@gmail.com>
2024-09-23 11:41:30 -0400

    updating base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://prashantchanne12.github.io",
-  base: "/",
+  base: "/portfolio",
 });
