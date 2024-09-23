05129f461d6d28cae363d28cdf9e106af1bf4967
Prashant Channe <prashantchanne121@gmail.com>
2024-09-22 22:53:07 -0400

    Added resume button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 const Navbar = () => {
   return (
-    <div className="py-5 px-2">
+    <div className="py-5 px-2 flex items-center">
       <span className="font-semibold text-white text-xl tracking-wider bg-black p-2 rounded">
         PC
       </span>
+      <div className="text-lg font-semibold"></div>
     </div>
   );
 };
