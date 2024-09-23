9326375af525a04640ffa5a67bfbe7cc62c72dec
Prashant Channe <prashantchanne121@gmail.com>
2024-09-22 22:35:30 -0400

    updated banner

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="py-5 px-2">
+      <span className="font-semibold text-white text-xl tracking-wider bg-black p-2 rounded">
+        PC
+      </span>
+    </div>
+  );
+};
+
+export default Navbar;
